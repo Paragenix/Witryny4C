f97821aaf9afab31b087ed7d37b759ea857fe52d
Paragenix <patrykkubu@gmail.com>
2023-09-15 09:56:04 +0200

    Create PHP index lekcja 1.php

---
+++ PHP index lekcja 1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhP składnia</title>
+</head>
+<body>
+<?php echo "Tu funkcja echo" ?>
+<br>
+<?php echo " Tu" . " funkcja" . " echo" ?>
+<br>
+<?php echo  ' Tu' . ' funkcja'  .  ' echo' ?>
+<br>
+<?php print "Tu funkcja print" ?>
+<br>
+<?= 'Oby pis wygral wybory' ?> 
+<br>
+<?= 'Oby Konfederacja wygrała wybory są najlepszą partią' ?> 
+
+    
+</body>
+</html>
